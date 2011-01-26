4f48418d9933c44d05cf48b971a23b6544151b6d
Chris McDevitt <chrism@pirc.co.uk>
2011-01-26 16:09:55 +0000

    Update deps. (prior to moving queue viewer to aleph)

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
-(defproject compojure "0.3.2"
+(defproject compojure "0.3.2-JETTY7"
   :description "A concise web framework for Clojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [commons-codec "1.3"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [commons-codec "1.4"] 
                  [commons-io "1.4"]
-                 [commons-fileupload "1.2.1"]
-                 [org.mortbay.jetty/jetty "6.1.21"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
-  :namespaces [compojure
+                 [commons-fileupload "1.2.1"] 
+                 [org.eclipse.jetty/jetty-server "7.2.2.v20101205"]
+                 [org.eclipse.jetty/jetty-servlet "7.2.2.v20101205"]
+                 [org.eclipse.jetty/jetty-websocket "7.2.2.v20101205"]]
+  ;:dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :aot [compojure
                compojure.control
                compojure.html
                compojure.html.gen
